b85ff1fa24b3b2e6195dbcaa57746e25b0f22deb
erichewan <erichewan@tencent.com>
2019-06-16 21:13:28 +0800

    init

---
+++ EWWebViewProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EWWebViewProgress
+//
+//  Created by EricWan on 2019/6/16.
+//  Copyright © 2019 EricWan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EWWebViewProgress/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  EWWebViewProgress
+//
+//  Created by EricWan on 2019/6/16.
+//  Copyright © 2019 EricWan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
